71db084f442ec47c7ec704eded1068f6b17c67db
taliazwennis <taliazwennis@gmail.com>
2023-03-14 00:34:12 +0200

    fix email validation

--- db/userModel.js
+++ db/userModel.js
@@ -1,5 +1,10 @@
 const mongoose = require("mongoose");
 
+var validateEmail = function(email) {
+  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return re.test(email)
+};
+
 const UserSchema = new mongoose.Schema({
   email: {
     type: String,
